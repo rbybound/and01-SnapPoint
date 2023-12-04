3d7efdc59f44d9022d5ddbdf474b360ea97b0c5c
rbybound <ruviiit@gmail.com>
2023-12-04 18:16:51 +0900

    [FEAT] 구독 화면 뷰모델 생성

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/subscription/SubscriptionFragment.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/subscription/SubscriptionFragment.kt
@@ -3,17 +3,24 @@ package com.boostcampwm2023.snappoint.presentation.subscription
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.boostcampwm2023.snappoint.R
 import com.boostcampwm2023.snappoint.databinding.FragmentSubscriptionBinding
 import com.boostcampwm2023.snappoint.presentation.base.BaseFragment
 import com.boostcampwm2023.snappoint.presentation.createpost.CreatePostActivity
+import com.boostcampwm2023.snappoint.presentation.main.MainViewModel
 
 class SubscriptionFragment : BaseFragment<FragmentSubscriptionBinding>(R.layout.fragment_subscription) {
+
+    private val activityViewModel: MainViewModel by activityViewModels()
+    private val viewModel: SubscriptionViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initBinding()
+
     }
 
     private fun initBinding() {
---
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/subscription/SubscriptionViewModel.kt
@@ -0,0 +1,18 @@
+package com.boostcampwm2023.snappoint.presentation.subscription
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SubscriptionViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
