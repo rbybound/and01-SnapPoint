c4b01ac6bf6206f9310ed4b03a982543738f549a
rbybound <ruviiit@gmail.com>
2023-12-04 22:22:32 +0900

    [FEAT] Room 모듈 Provider 생성

---
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/di/LocalModule.kt
@@ -0,0 +1,44 @@
+package com.boostcampwm2023.snappoint.di
+
+import android.content.Context
+import androidx.room.Room
+import com.boostcampwm2023.snappoint.data.local.LocalPostDatabase
+import com.boostcampwm2023.snappoint.data.local.dao.LocalPostDao
+import com.boostcampwm2023.snappoint.data.local.datasource.LocalPostDataSource
+import com.boostcampwm2023.snappoint.data.repository.LocalPostRepository
+import com.boostcampwm2023.snappoint.data.repository.LocalPostRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalPostDatabase(@ApplicationContext context: Context): LocalPostDatabase =
+        Room.databaseBuilder(
+            context,
+            LocalPostDatabase::class.java,
+            "database",
+        ).build()
+
+    @Provides
+    @Singleton
+    fun provideLocalPostDao(localPostDatabase: LocalPostDatabase): LocalPostDao =
+        localPostDatabase.localPostDao()
+
+    @Provides
+    @Singleton
+    fun provideLocalPostDataSource(localPostDao: LocalPostDao): LocalPostDataSource =
+        LocalPostDataSource(localPostDao)
+
+    @Provides
+    @Singleton
+    fun provideLocalPostRepository(localPostDataSource: LocalPostDataSource): LocalPostRepository =
+        LocalPostRepositoryImpl(localPostDataSource)
+}
