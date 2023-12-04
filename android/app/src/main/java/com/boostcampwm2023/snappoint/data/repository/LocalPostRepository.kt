d84d2ce9bdba6ea3fd7d17c69ac11d0e5962dab6
rbybound <ruviiit@gmail.com>
2023-12-04 23:42:56 +0900

    [FEAT] repository에서 PostSummaryState, SerializedPost 변환

--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/LocalPostRepository.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/LocalPostRepository.kt
@@ -1,10 +1,11 @@
 package com.boostcampwm2023.snappoint.data.repository
 
 import com.boostcampwm2023.snappoint.data.local.entity.SerializedPost
+import com.boostcampwm2023.snappoint.presentation.model.PostSummaryState
 import kotlinx.coroutines.flow.Flow
 
 interface LocalPostRepository {
 
-    fun getLocalPosts(): Flow<List<SerializedPost>>
-    suspend fun insertPosts(serializedPost: SerializedPost)
+    fun getLocalPosts(): Flow<List<PostSummaryState>>
+    suspend fun insertPosts(postSummaryState: PostSummaryState)
 }
--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/LocalPostRepositoryImpl.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/LocalPostRepositoryImpl.kt
@@ -2,18 +2,28 @@ package com.boostcampwm2023.snappoint.data.repository
 
 import com.boostcampwm2023.snappoint.data.local.datasource.LocalPostDataSource
 import com.boostcampwm2023.snappoint.data.local.entity.SerializedPost
+import com.boostcampwm2023.snappoint.data.mapper.asPostSummaryState
+import com.boostcampwm2023.snappoint.data.mapper.asSerializedPost
+import com.boostcampwm2023.snappoint.presentation.model.PostSummaryState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class LocalPostRepositoryImpl @Inject constructor(
     private val localPostDataSource: LocalPostDataSource
 ) : LocalPostRepository {
 
-    override fun getLocalPosts(): Flow<List<SerializedPost>> {
+    override fun getLocalPosts(): Flow<List<PostSummaryState>> {
         return localPostDataSource.getLocalPosts()
+            .map { serializedPosts ->
+                serializedPosts.map { serializedPost ->
+                    serializedPost.asPostSummaryState()
+                }
+            }
     }
 
-    override suspend fun insertPosts(serializedPost: SerializedPost) {
-        localPostDataSource.insertPosts(listOf(serializedPost))
+    override suspend fun insertPosts(postSummaryState: PostSummaryState) {
+        localPostDataSource.insertPosts(listOf(postSummaryState.asSerializedPost()))
     }
 }
