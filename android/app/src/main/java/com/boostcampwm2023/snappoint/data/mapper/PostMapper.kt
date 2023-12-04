b8472cebbd4a081ef84cfcc3e2857e1eac91e6c3
LJG7123 <ljg7123@naver.com>
2023-12-04 22:16:31 +0900

    [REFACTOR] 코드 리팩토링

--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/mapper/PostMapper.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/mapper/PostMapper.kt
@@ -51,7 +51,7 @@ fun PostBlockState.asPostBlock(): PostBlock {
         }
         is PostBlockState.IMAGE -> {
             PostBlock(
-                content = this.content,
+                content = this.description,
                 type = BlockType.MEDIA.type,
                 latitude = this.position.latitude,
                 longitude = this.position.longitude,
